8a4c5460b3f008542c6e43c88425a243b542cc6e
moon <nowstart2691@gmail.com>
2025-01-15 22:01:29 +0900

    feat(v1.0.0): 스프링 어드민 서버 구축

--- src/main/java/org/nowstart/admin/AdminServiceApplication.java
+++ src/main/java/org/nowstart/admin/AdminServiceApplication.java
@@ -1,10 +1,12 @@
-package org.nowstart.adminservice;
+package org.nowstart.admin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableAdminServer
+@EnableEurekaServer
 @SpringBootApplication
 public class AdminServiceApplication {
 
---
+++ src/main/java/org/nowstart/admin/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package org.nowstart.admin.config;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final AdminServerProperties adminServer;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.headers(headersConfigurer -> headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+            .csrf(csrf -> csrf.ignoringRequestMatchers("/**"))
+            .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                .requestMatchers(new AntPathRequestMatcher("/eureka/**"))
+                .permitAll()
+                .requestMatchers(new AntPathRequestMatcher(adminServer.path("/assets/**")))
+                .permitAll()
+                .requestMatchers(new AntPathRequestMatcher(adminServer.path("/login")))
+                .permitAll()
+                .anyRequest()
+                .authenticated())
+            .formLogin(formLogin -> formLogin.loginPage(adminServer.path("/login"))
+                .defaultSuccessUrl(adminServer.getContextPath(), true))
+            .logout(logout -> logout.logoutUrl(adminServer.path("/logout")))
+            .rememberMe(rememberMe -> rememberMe.key(UUID.randomUUID().toString()).tokenValiditySeconds(1209600))
+            .httpBasic(Customizer.withDefaults())
+            .build();
+    }
+}
