d705aa23a0ea003002998a25833b46172c9a04ea
moon <nowstart2691@lguplus.co.kr>
2025-06-26 08:43:02 +0900

    feat: AuditEvent 추가

--- src/main/java/org/nowstart/admin/AdminServiceApplication.java
+++ src/main/java/org/nowstart/admin/AdminServiceApplication.java
@@ -2,8 +2,11 @@ package org.nowstart.admin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.audit.AuditEventRepository;
+import org.springframework.boot.actuate.audit.InMemoryAuditEventRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableAdminServer
 @EnableDiscoveryClient
@@ -14,4 +17,8 @@ public class AdminServiceApplication {
         SpringApplication.run(AdminServiceApplication.class, args);
     }
 
+    @Bean
+    public AuditEventRepository createAuditEventRepository() {
+        return new InMemoryAuditEventRepository();
+    }
 }
