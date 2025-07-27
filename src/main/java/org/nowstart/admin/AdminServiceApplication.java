51e406e778b3d2a6c07703d1861489a29fa6d79c
moon <nowstart2691@gmail.com>
2025-07-27 16:22:22 +0900

    feat: admin 시큐리티 제거

--- src/main/java/org/nowstart/admin/AdminServiceApplication.java
+++ src/main/java/org/nowstart/admin/AdminServiceApplication.java
@@ -2,11 +2,8 @@ package org.nowstart.admin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.audit.AuditEventRepository;
-import org.springframework.boot.actuate.audit.InMemoryAuditEventRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 
 @EnableAdminServer
 @EnableDiscoveryClient
@@ -15,10 +12,5 @@ public class AdminServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AdminServiceApplication.class, args);
-    }
-
-    @Bean
-    public AuditEventRepository createAuditEventRepository() {
-        return new InMemoryAuditEventRepository();
     }
 }
